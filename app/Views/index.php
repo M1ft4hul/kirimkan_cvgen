265b0d69a7f8701593524175a3ad1b840a818154
M1ft4hul <mift724@gmail.com>
2024-01-27 22:23:41 +0800

    cookie consent CLEAR

--- app/Views/index.php
+++ app/Views/index.php
@@ -423,6 +423,21 @@
 
     <!-- Template Javascript -->
     <script src="<?= base_url() ?>/js/main.js"></script>
+    <script type="text/javascript" src="//www.privacypolicies.com/public/cookie-consent/4.1.0/cookie-consent.js" charset="UTF-8"></script>
+    <script type="text/javascript" charset="UTF-8">
+        document.addEventListener('DOMContentLoaded', function() {
+            cookieconsent.run({
+                "notice_banner_type": "simple",
+                "consent_type": "express",
+                "palette": "dark",
+                "language": "en",
+                "page_load_consent_levels": ["strictly-necessary"],
+                "notice_banner_reject_button_hide": false,
+                "preferences_center_close_button_hide": false,
+                "page_refresh_confirmation_buttons": false
+            });
+        });
+    </script>
 </body>
 
 </html>
